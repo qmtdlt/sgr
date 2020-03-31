e1315caf51ec05508c656502d038e1ee12eb4ecd
qmtdlt <qmtdlt@gmail.com>
2020-03-31 22:07:32 +0800

    添加项目文件。

---
+++ Controllers/CountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace sgr.Controllers
+{
+    [Route("api/count")]
+    public class CountController : Controller
+    {
+        private readonly IHubContext<CountHub> _countHub;
+        public CountController(IHubContext<CountHub> countHub)
+        {
+            _countHub = countHub;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            await _countHub.Clients.All.SendAsync("someFunc",new { random = "abcd"});
+            return Accepted(1);
+        }
+    }
+}
---
+++ CountHub.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sgr
+{
+    public class CountHub:Hub
+    {
+        private readonly CountService _countService;
+        public CountHub(CountService countService)
+        {
+            _countService = countService;
+        }
+        public async Task GetLatestCount(string random)
+        {
+            int count;
+            do
+            {
+                count = _countService.GetLatestCount();
+                Thread.Sleep(1000);
+                await Clients.All.SendAsync("ReceiveUpdate", count);
+            }
+            while (count < 10);
+            await Clients.All.SendAsync("Finished");
+        }
+        public override async Task OnConnectedAsync()
+        {
+            //var connectedId = Context.ConnectionId;
+            //var client = Clients.Client(connectedId);
+            //await client.SendAsync("SomeFunc",new { });
+            //await Clients.AllExcept(connectedId).SendAsync("SomeFunc", new { });
+            //await Groups.AddToGroupAsync(connectedId, "MyGroup");
+            //await Groups.RemoveFromGroupAsync(connectedId, "MyGroup");
+            //await Clients.Group("MyGroup").SendAsync("SomeFunc");
+        }
+    }
+}
